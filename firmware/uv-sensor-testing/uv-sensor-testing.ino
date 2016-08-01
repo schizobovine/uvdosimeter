a0fc3008c777eda8d598eef2f607295ab620a19e
Sean Caulfield <sean@yak.net>
2016-08-01 16:17:39 -0400

    testing against other sensors

--- firmware/uv-sensor-testing/uv-sensor-testing.ino
+++ firmware/uv-sensor-testing/uv-sensor-testing.ino
@@ -17,7 +17,6 @@
  */
 
 #include <Arduino.h>
-#include <Adafruit_SSD1306.h>
 #include <Adafruit_SI1145.h>
 #include <Adafruit_VEML6070.h>
 #include <VEML6075.h>
@@ -25,25 +24,38 @@
 #include <RTClib.h>
 #include <DS3231.h>
 
+//#include <Adafruit_BLE.h>
+//#include <Adafruit_BluefruitLE_SPI.h>
+
+#define LOG_FILENAME "uv_raw.log"
+
 const uint8_t SD_CS     = 10;
 const uint8_t LOGMODE   = O_WRITE | O_CREAT | O_APPEND;
-const uint8_t OLED_RST  = 6;
-//#define LOG_FILENAME "uvsensors.log"
-//#define LOG_RAW_FILENAME "uv_raw.log"
-#define LOG_FILENAME "uv_raw.log"
+
+const uint8_t BLE_CS    = 8;
+const uint8_t BLE_IRQ   = 7;
+const uint8_t BLE_RST   = 4;
+
+const uint8_t GUVA12_PIN = A2;
+const uint8_t ML8511_PIN = A1;
 
 Adafruit_SI1145 si1145 = Adafruit_SI1145();
 Adafruit_VEML6070 veml6070 = Adafruit_VEML6070();
 VEML6075 veml6075 = VEML6075();
-
 RTC_DS3231 rtc = RTC_DS3231();
-Adafruit_SSD1306 display(OLED_RST);
+
+//Adafruit_BluefruitLE_SPI modem(BLE_CS, BLE_IRQ, BLE_RST);
 
 File logfile;
 uint8_t lastmin = 0;
 
 uint16_t guva_reading;
+uint16_t ml8511_reading;
+
 float si1145_index = 0.0;
+uint16_t si1145_vis = 0;
+uint16_t si1145_ir = 0;
+
 float veml6070_index = 0.0;
 float veml6075_uva = 0.0;
 float veml6075_uvb = 0.0;
@@ -57,164 +69,76 @@ uint16_t veml6075_raw_vis = 0;
 uint16_t veml6075_raw_ir = 0;
 
 void error(const char *msg, int pause=1000) {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.print(msg);
-  display.display();
+  Serial.println(msg);
   if (pause > 0) {
     delay(pause);
   }
 }
 
-float getUVIndex(uint16_t uv_reading) {
+float getUVIndexGUVA12(uint16_t uv_reading) {
   return (uv_reading / 1024.0 * 5.0) / 0.1;
 }
 
-void refreshDisplay(DateTime *dt) {
-
-  display.clearDisplay();
-
-  display.setCursor(0, 0);
-  display.print(F("GUVA "));
-  display.setCursor(32, 0);
-  display.print(getUVIndex(guva_reading), 1);
-
-  display.setCursor(0, 8);
-  display.print(F("SI45 "));
-  display.setCursor(32, 8);
-  display.print(si1145_index, 1);
-
-  display.setCursor(0, 16);
-  display.print(F("VL70 "));
-  display.setCursor(32, 16);
-  display.print(veml6070_index, 1);
-
-  display.setCursor(0, 24);
-  display.print(F("VL75 "));
-  display.setCursor(32, 24);
-  display.print(veml6075_index, 1);
-
-  display.setCursor(64, 0);
-  display.print(dt->year());
-  if (dt->month() < 10) {
-    display.print(F("-0"));
-  } else {
-    display.print(F("-"));
-  }
-  display.print(dt->month());
-  if (dt->day() < 10) {
-    display.print(F("-0"));
-  } else {
-    display.print(F("-"));
-  }
-  display.print(dt->day());
-
-  display.setCursor(64, 8);
-  if (dt->hour() < 10) {
-    display.print(F("0"));
-  }
-  display.print(dt->hour());
-  if (dt->minute() < 10) {
-    display.print(F(":0"));
-  } else {
-    display.print(F(":"));
-  }
-  display.print(dt->minute());
-  if (dt->second() < 10) {
-    display.print(F(":0"));
-  } else {
-    display.print(F(":"));
-  }
-  display.print(dt->second());
-
-  display.display();
-
+float getUVIndexML8511(uint16_t uv_reading) {
+  return (uv_reading / 1024.0 * 5.0) / 0.1;
 }
 
-void timestamp(DateTime *dt, File *f) {
+void timestamp(DateTime *dt, Stream *f) {
   if (dt && f) {
-
-    f->print(dt->year());
-    if (dt->month() < 10) {
-      f->print(F("-0"));
-    } else {
-      f->print(F("-"));
-    }
-    f->print(dt->month(), 2);
-    if (dt->day() < 10) {
-      f->print(F("-0"));
-    } else {
-      f->print(F("-"));
-    }
-    f->print(dt->day());
-
-    f->print(F("T"));
-
-    if (dt->hour() < 10) {
-      f->print(F("0"));
-    }
-    f->print(dt->hour());
-    if (dt->minute() < 10) {
-      f->print(F(":0"));
-    } else {
-      f->print(F(":"));
-    }
-    f->print(dt->minute());
-    if (dt->second() < 10) {
-      f->print(F(":0"));
-    } else {
-      f->print(F(":"));
-    }
-    f->print(dt->second());
-
+    String s;
+    dt->iso8601(s);
+    f->print(s);
   }
 }
 
-void logRawData(DateTime *dt) {
-  if (logfile) {
-    timestamp(dt, &logfile);
-    logfile.print(F(","));
-    logfile.print(guva_reading, 1);     logfile.print(F(","));
-    logfile.print(si1145_index, 1);   logfile.print(F(","));
-    logfile.print(veml6070_raw_uv, 1); logfile.print(F(","));
-    logfile.print(veml6075_raw_uva, 1);   logfile.print(F(","));
-    logfile.print(veml6075_raw_uvb, 1);   logfile.print(F(","));
-    logfile.print(veml6075_raw_dark, 1); logfile.println(F(","));
-    logfile.print(veml6075_raw_vis, 1); logfile.println(F(","));
-    logfile.print(veml6075_raw_ir, 1); logfile.println(F(","));
-  }
-}
+void logData(DateTime *dt, Stream *out) {
+  if (out) {
 
-void logData(DateTime *dt) {
-  if (logfile) {
-    timestamp(dt, &logfile);
-    logfile.print(F(","));
-    logfile.print(guva_reading, 1);     logfile.print(F(","));
-    logfile.print(si1145_index, 1);   logfile.print(F(","));
-    logfile.print(veml6070_index, 1); logfile.print(F(","));
-    logfile.print(veml6075_uva, 1);   logfile.print(F(","));
-    logfile.print(veml6075_uvb, 1);   logfile.print(F(","));
-    logfile.print(veml6075_index, 1); logfile.println(F(","));
+    timestamp(dt, out);
+    out->print(F(","));
+
+    out->print(guva_reading, 1);                      out->print(F(","));
+    out->print(getUVIndexGUVA12(guva_reading), 1);    out->print(F(","));
+
+    out->print(ml8511_reading, 1);                    out->print(F(","));
+    out->print(getUVIndexML8511(ml8511_reading), 1);  out->print(F(","));
+
+    out->print(si1145_index, 1);                      out->print(F(","));
+    out->print(si1145_vis);                           out->print(F(","));
+    out->print(si1145_ir);                            out->print(F(","));
+
+    out->print(veml6070_raw_uv);                      out->print(F(","));
+    out->print(veml6070_index, 1);                    out->print(F(","));
+
+    out->print(veml6075_raw_uva);                     out->print(F(","));
+    out->print(veml6075_raw_uvb);                     out->print(F(","));
+    out->print(veml6075_raw_dark);                    out->print(F(","));
+    out->print(veml6075_raw_vis);                     out->print(F(","));
+    out->print(veml6075_raw_ir);                      out->print(F(","));
+    out->print(veml6075_index, 1);                    out->print(F(","));
+
+    out->println();
+
   }
 }
 
 void setup() {
+
+  // Setup serial
+  while (!Serial)
+    ;
+  Serial.begin(115200);
+  Serial.println(F("Hello, UV dosimeter tester starting..."));
+
+  // Setup GUVA-S12D & ML8511
+  pinMode(GUVA12_PIN, INPUT);
+  pinMode(ML8511_PIN, INPUT);
 
   // Initialize i2c bus
   Wire.begin();
 
   // Init RTC
   rtc.begin();
-
-  // Setup display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.display();
-
-  // Setup GUVA-S12D
-  pinMode(A1, INPUT);
 
   // Setup Si1145
   si1145.begin();
@@ -227,26 +151,44 @@ void setup() {
 
   // Open logfile
   pinMode(SD_CS, OUTPUT);
-  //if (!SD.begin(SD_CS, SD_MOSI, SD_MISO, SD_SCK)) {
   if (!SD.begin(SD_CS)) {
     error("MicroSD not found!");
   } else if (!(logfile = SD.open(F(LOG_FILENAME), LOGMODE))) {
     error("Could not open " LOG_FILENAME "!");
-  //} else if (!(lograw = SD.open(F(LOG_RAW_FILENAME), LOGMODE))) {
-  //  error("Could not open " LOG_RAW_FILENAME "!");
   }
+
+  // Setup BLE modem
+  //if (!modem.begin(false)) {
+  //  error("Starting BLE modem failed!");
+  //}
 
 }
 
 void getRawData() {
 
-  veml6070_raw_uv = veml6070.readUV();
+  // Read analog sensors
+  guva_reading = analogRead(GUVA12_PIN);
+  ml8511_reading = analogRead(ML8511_PIN);
 
+  // Read Si1145
+  si1145_index = si1145.readUV();
+  si1145_vis = si1145.readVisible();
+  si1145_ir = si1145.readIR();
+
+  // Read VEML6070
+  veml6070_raw_uv = veml6070.readUV();
+  veml6070_index = veml6070.getUVIndex();
+
+  // Read VEML6075
+  veml6075.poll();
   veml6075_raw_uva = veml6075.getRawUVA();
   veml6075_raw_uvb = veml6075.getRawUVA();
   veml6075_raw_dark = veml6075.getRawDark();
   veml6075_raw_vis = veml6075.getRawVisComp();
   veml6075_raw_ir = veml6075.getRawIRComp();
+  veml6075_uva = veml6075.getUVA();
+  veml6075_uvb = veml6075.getUVB();
+  veml6075_index = veml6075.getUVIndex();
 
 }
 
@@ -257,37 +199,31 @@ void loop() {
   //
   DateTime now = rtc.now();
 
+  //
+  // Refresh data
+  //
   getRawData();
-  logRawData(&now);
-
-  // Read GUVA-S12D
-  guva_reading = analogRead(A1);
-
-  // Read Si1145
-  si1145_index = si1145.readUV();
-
-  // Read VEML6070
-  veml6070_index = veml6070.getUVIndex();
-
-  // Read VEML6075
-  veml6075.poll();
-  veml6075_uva = veml6075.getUVA();
-  veml6075_uvb = veml6075.getUVB();
-  veml6075_index = veml6075.getUVIndex();
 
   //
-  // Display results
+  // Display results over serial and ble
   //
-  refreshDisplay(&now);
+  logData(&now, &Serial);
+  //if (modem.isConnected()) {
+  //  logData(&now, &modem);
+  //}
+
+  //
+  // Write out data to SD card every minute or so
+  //
+  if (now.minute() != lastmin) {
+    lastmin = now.minute();
+    logData(&now, &logfile);
+    logfile.flush();
+  }
 
   //
   // Log to file every minute
   //
-  if (now.minute() != lastmin) {
-    lastmin = now.minute();
-    logData(&now);
-  }
-
   delay(1000);
 
 }
